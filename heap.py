3c238c253437323217bcca53e807522a01a7c3cd
Angelika Ostrowska <01178572@pw.edu.pl>
2023-04-23 16:05:54 +0200

    Implemented printing heap

--- heap.py
+++ heap.py
@@ -4,13 +4,14 @@
 # [8734202]
 
 #  ^ ________^
-#  /         \  
+#  /         \
 # |  -     -  |
 # |     ⌔     |
 # |      ⌒    |
 #  \_________/
 #     ↑↑↑
-#     KRET 
+#     KRET
+from math import *
 
 def push_2(el, heap:list):
     heap.append(el)
@@ -53,6 +54,28 @@ def make_heap_2(list):
         heap = push_2(el, heap)
     return heap
 
+def print_heap(heap:list):
+    n_of_levels = ceil(log(len(heap), 2))
+    max_len = 2**(n_of_levels-1)
+    for i in range(n_of_levels):
+        level_start = (2**i)-1
+        level_end = (2**(i+1)-1)
+        fill_level = []
+        if len(heap) <= level_end:
+            fill_level = [" " for j in range(level_end-len(heap)+1)]
+            level_end = len(heap)
+        level = heap[level_start:level_end] + fill_level
+        level = [str(num) for num in level]
+        print((" ".join(level)).center(max_len*2-1, " "))
+    # 0:1
+    # 1:3
+    # 3:7
+    #len=7
+    #7-7+1
+    #len=6 7-6+1
 print("aaa")
-heap = make_heap_2([5,2,4,1,0,3])
+# heap = make_heap_2([5,2,4,1,0,3])
+heap = make_heap_2([5,2,4,1,0,3,9,5,7,1])
+print(heap)
+print_heap(heap)
 
