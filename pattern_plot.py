7ffb26ae900af63ddd92768cbf65bfbc77f8abb4
Angelika Ostrowska <01178572@pw.edu.pl>
2023-05-03 19:08:04 +0200

    Removed fragment which existerd for test purpose

--- pattern_plot.py
+++ pattern_plot.py
@@ -46,12 +46,12 @@ def compare_plot(text):
     fig, ax = plt.subplots()
 
     x = np.linspace(100, 1001, 10)
-    # naive = measure(x, text, find_naive)
-    naive = [8.421875, 16.71875, 25.59375, 45.875, 49.515625, 49.3125, 56.6875, 65.1875, 65.3125, 68.65625]
-    # kmp = measure(x, text, find_KMP)
-    kmp = [13.09375, 38.234375, 60.53125, 51.21875, 62.125, 73.546875, 103.890625, 98.234375, 109.1875, 124.875]
-    # kr = measure(x, text, find_KR)
-    kr = [89.96875, 154.859375, 246.0, 326.875, 438.96875, 524.953125, 612.921875, 695.859375, 777.421875, 868.875]
+    naive = measure(x, text, find_naive)
+    # naive = [8.421875, 16.71875, 25.59375, 45.875, 49.515625, 49.3125, 56.6875, 65.1875, 65.3125, 68.65625]
+    kmp = measure(x, text, find_KMP)
+    # kmp = [13.09375, 38.234375, 60.53125, 51.21875, 62.125, 73.546875, 103.890625, 98.234375, 109.1875, 124.875]
+    kr = measure(x, text, find_KR)
+    # kr = [89.96875, 154.859375, 246.0, 326.875, 438.96875, 524.953125, 612.921875, 695.859375, 777.421875, 868.875]
 
     ax.plot(x, naive, linewidth=2.0, label='NAIVE')
     ax.plot(x, kmp, linewidth=2.0, label='KMP')
@@ -71,8 +71,8 @@ def compare_plot(text):
 def main():
     with open('pan-tadeusz.txt','r',encoding="utf-8") as file_handle:
         text = file_handle.read()
-    # make_plot(text, 2)
-    compare_plot(text)
+    make_plot(text, 3)
+    # compare_plot(text)
 
 
 
